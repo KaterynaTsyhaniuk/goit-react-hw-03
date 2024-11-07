988c0104f8d67b65e890ffa0e875d127bd763af3
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2024-11-07 22:16:25 +0200

    add 4-step hw-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import ContactList from "./ContactList/ContactList";
 import SearchBox from "./SearchBox/SearchBox";
 import "./App.css";
+import ContactForm from "./ContactForm/ContactForm";
+import { nanoid } from "nanoid";
 
 function App() {
   const [contactList, setContactList] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    { id: nanoid(), name: "Rosie Simpson", number: "459-12-56" },
+    { id: nanoid(), name: "Hermione Kline", number: "443-89-12" },
+    { id: nanoid(), name: "Eden Clements", number: "645-17-79" },
+    { id: nanoid(), name: "Annie Copeland", number: "227-91-26" },
   ]);
 
   const [inputValue, setInputValue] = useState("");
@@ -19,12 +21,21 @@ function App() {
   };
 
   const filteredContacts = contactList.filter((contact) =>
-    contact.name.toLowerCase().includes(inputValue.toLowerCase())
+    contact?.name?.toLowerCase().includes(inputValue.toLowerCase())
   );
+
+  const handleAddContact = (newContact) => {
+    setContactList((prevContacts) => [
+      ...prevContacts,
+      { ...newContact, id: nanoid() },
+    ]);
+  };
 
   return (
     <div>
       <h1>Phonebook</h1>
+
+      <ContactForm onAddContact={handleAddContact} />
       <SearchBox inputValue={inputValue} handleChange={handleChange} />
       <ContactList contacts={filteredContacts} />
     </div>
